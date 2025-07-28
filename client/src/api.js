90b6a8621a15d572226743a000351a83e91b5ddd
shrey2109 <spparikh2109@gmail.com>
2025-07-29 00:01:20 +0530

    updated backend endpoint url for frontend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // Change when deployed
+  baseURL: "http://flytbase-drone-system-production.up.railway.app/api", // Change when deployed
 });
 
 export default api;
